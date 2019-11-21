677be2ce02bbe20fce4acb8ae5884f0b44f2723d
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-21 11:09:25 -0600

    dispatch function in store

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
+import { fetchMemes } from './actions';
 
 const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(fetchMemes());
 
 ReactDOM.render(
   <Provider store={store}>
