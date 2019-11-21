f6eb9271810eb2b60fbc46932b2d5092783af5df
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2019-11-21 11:23:21 -0600

    fetch data from api

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 import { fetchMemes } from './actions';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 store.subscribe(() => console.log('store', store.getState()));
 store.dispatch(fetchMemes());
 
